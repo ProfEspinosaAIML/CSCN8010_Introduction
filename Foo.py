2ed81c2728023a3dfb8e5480281d4d6d96ef2b7d
AIML <eespinosa@conestogac.on.ca>
2025-05-08 14:21:09 -0400

    Add HelloPython class that prints hello world

---
+++ Foo.py
@@ -0,0 +1,10 @@
+class HelloPython:
+    def __init__(self):
+        self.say_hello()
+
+    def say_hello(self):
+        print("hello world")
+
+# Example usage:
+if __name__ == "__main__":
+    greeter = HelloPython()
